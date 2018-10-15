0ce0ddfb6b5a890eec71bb5f9655f888dd389527
gorushkin <gorushkin@gmail.com>
2018-10-15 16:57:02 +0300

    sweet home

--- source/js/catalog.js
+++ source/js/catalog.js
@@ -1,34 +1,15 @@
 var myList = document.querySelectorAll(".product__add-to");
 var popup = document.querySelector(".add-to-basket");
 var overlay = document.querySelector(".overlay");
-// var link = document.querySelector(".product-card__to-order");
 
-// link.addEventListener("click", function (evt) {
-//   evt.preventDefault();
-//   popup.classList.add("add-to-basket--show");
-//   overlay.classList.add("overlay--show");
-// });
-
-// for (var i = 0; i < myList.length, i++;) {
-//   myList[i].addEventListener("click", function (evt) {
-//     evt.preventDefault();
-//     console.log('press');
-//     // popup.classList.add("add-to-basket--show");
-//     // overlay.classList.add("overlay--show");
-//   });
-// }
-
-for (var i = 0; i < myList.length, i = i + 1) {
-  console.log(i);
+for (var i = 0; i < myList.length; i++) {
+  myList[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    console.log('press');
+    popup.classList.add("add-to-basket--show");
+    overlay.classList.add("overlay--show");
+  });
 }
-
-
-
-
-
-
-
-
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
