4010219872225aaac96bd61b9425035dcbb2ae26
gorushkin <gorushkin@gmail.com>
2018-10-15 14:10:13 +0300

    overlay insert

--- source/js/script.js
+++ source/js/script.js
@@ -12,11 +12,13 @@ button.addEventListener("click", function (evt) {
 });
 
 var popup = document.querySelector(".add-to-basket");
+var overlay = document.querySelector(".overlay");
 var link = document.querySelector(".product-card__to-order");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("add-to-basket--show");
+  overlay.classList.add("overlay--show");
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -26,5 +28,19 @@ window.addEventListener("keydown", function (evt) {
     if (popup.classList.contains("add-to-basket--show")) {
       popup.classList.remove("add-to-basket--show");
     }
+    if (overlay.classList.contains("overlay--show")) {
+      overlay.classList.remove("overlay--show");
+    }
   }
 })
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (popup.classList.contains("add-to-basket--show")) {
+    popup.classList.remove("add-to-basket--show");
+  }
+  if (overlay.classList.contains("overlay--show")) {
+    overlay.classList.remove("overlay--show");
+  }
+});
+
