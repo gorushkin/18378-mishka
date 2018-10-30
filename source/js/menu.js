7e3a79b4fdd08b9a615b4d6de348632a026388bc
gorushkin <gorushkin@users.noreply.github.com>
2018-10-30 19:21:57 +0300

    pp

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,13 +1,13 @@
 var button = document.querySelector(".page-header__toggle");
 button.classList.add("page-header__toggle--show");
-var sitemenu = document.querySelector(".page-header__main-nav");
-sitemenu.classList.add("page-header__main-nav--close-menu");
-var usermenu = document.querySelector(".page-header__user-list");
-usermenu.classList.add("page-header__user-list--close-menu");
+var sitemenu = document.querySelector(".page-header__main-nav-wrapper");
+sitemenu.classList.add("page-header__main-nav-wrapper--close-menu");
+var usermenu = document.querySelector(".page-header__user-list-wrapper");
+usermenu.classList.add("page-header__user-list-wrapper--close-menu");
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   button.classList.toggle("page-header__toggle--close");
-  sitemenu.classList.toggle("page-header__main-nav--show-menu");
-  usermenu.classList.toggle("page-header__user-list--show-menu");
+  sitemenu.classList.toggle("page-header__main-nav-wrapper--show-menu");
+  usermenu.classList.toggle("page-header__user-list-wrapper--show-menu");
 });
